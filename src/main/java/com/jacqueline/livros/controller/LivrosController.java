9927cb24f0ae5398ffac7e90269045968e00a9a9
Jacqueline Marinho <jacque_marinho@hotmail.com>
2020-09-02 21:50:14 -0300

    Criando método Delete

--- src/main/java/com/jacqueline/livros/controller/LivrosController.java
+++ src/main/java/com/jacqueline/livros/controller/LivrosController.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -68,7 +69,7 @@ public class LivrosController
 	
 
 	@GetMapping("/{id}")
-	public DetalhesLivroDto listarLivro(@PathVariable Long id)
+	public DetalhesLivroDto listarUmLivro(@PathVariable Long id)
 	{
 		Livro livro = livroRepository.getOne(id);
 		return new DetalhesLivroDto(livro);
@@ -81,6 +82,13 @@ public class LivrosController
 		Livro livro = form.atualizar(id, livroRepository);
 		return ResponseEntity.ok(new LivroDto(livro));
 		
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity deletar(@PathVariable Long id)
+	{
+		livroRepository.deleteById(id);
+		return ResponseEntity.ok().build(); 
 	}
 	
 	
